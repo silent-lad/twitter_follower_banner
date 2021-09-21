9ddf04f640d057bea8e8041d243a59edf4fa9bf0
silent-lad <divyanshtripathi1998@gmail.com>
2021-09-21 20:31:04 +0530

    Listen server

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const chalk = require("chalk");
 
 const app = express();
 
+app.set("port", process.env.PORT || 8080);
+
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
@@ -19,26 +21,30 @@ app.get("/follow",(req,res)=>{
     res.json({status:follow_count});
 })
 
-const token = process.env.BEARER_TOKEN
-if (!token) {
-  terminate(
-    'Config mismatch. Expected BEARER_TOKEN environment variable to contain a Twitter API token. Found undefined',
-  )
-}
+app.listen(app.get("port"), () => {
+    console.log("Listeneing at port 5000");
+  });
 
-const auth_headers = {
-    headers: {
-       Authorization: `Bearer ${token}`,
-      'Content-type': 'application/json',
-    },
-    timeout: 20000,
-  }
-  var userId = "906370059097972737";
+// const token = process.env.BEARER_TOKEN
+// if (!token) {
+//   terminate(
+//     'Config mismatch. Expected BEARER_TOKEN environment variable to contain a Twitter API token. Found undefined',
+//   )
+// }
 
-  return needle('get', `https://api.twitter.com/1.1/users/show.json?user_id=${userId}`, {}, auth_headers).then(async (res) => {
-    if (
-      res.body 
-    ) {
-      console.log(res.body.followers_count);
-    }
-  })
+// const auth_headers = {
+//     headers: {
+//        Authorization: `Bearer ${token}`,
+//       'Content-type': 'application/json',
+//     },
+//     timeout: 20000,
+//   }
+//   var userId = "906370059097972737";
+
+//   return needle('get', `https://api.twitter.com/1.1/users/show.json?user_id=${userId}`, {}, auth_headers).then(async (res) => {
+//     if (
+//       res.body 
+//     ) {
+//       console.log(res.body.followers_count);
+//     }
+//   })
